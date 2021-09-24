badafae8e4617c7de115a8a34c12fe5a5a59e45a
CarolinaTov <89940895+CarolinaTov@users.noreply.github.com>
2021-09-24 15:50:06 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+ <html lang="es">
+ <head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Hoja de Vida</title>
+   <link  rel="stylesheet" href="ColorCss/index.css">
+   <link href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@1,700&display=swap" rel="stylesheet">
+   <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@200&display=swap" rel="stylesheet">
+ </head>
+ <body>
+
+   <div class="name">
+
+<div class="img">
+<img src="Ingeniera.jpg" alt="">
+</div>
+
+    <div class="content">
+      <p class="parrafo1"> Carolina Quimbayo Tovar </p>
+      <p class="parrafo2"> Estudiante Ingeniería Electrónica </p>
+    </div>
+</div>
+
+
+<div class="infoPersonal">
+  <p class="TituloIP"> Datos Personales </p>
+
+  <p class="InfoP"> Fecha de Nacimiento:    05 de Julio de 1999 </p>
+  <p class="InfoP"> Cédula de ciudadanía:     1.022.444.557 de Bogotá </p>   
+  <p class="InfoP"> Sexo:     Femenino </p>
+  <p class="InfoP"> Estado Civil:     Soltera </p>
+  <p class="InfoP"> Dirección:    Cra 68 i # 43 c 63 sur </p>   
+  <p class="InfoP"> Contacto:     +57 312 443 3757 </p>
+  <p class="InfoP"> Correo electrónico:     carolinaquimbayo@usantotomas.edu.co </p>
+</div>
+
+<div class="info">
+    <p class="Perfil"> Perfil </p>
+    <p class="ContenidoPerfil"> Soy una persona calmada, amable y paciente.
+    Me es fácil  tener buenas relaciones interpersonales con mis compañeros y disfruto del aprendizaje constante,
+    lo cual ha significado en mi trayectoria, una amplia ventaja en como presto toda mi atención en nuevos proyectos 
+    y el mejoramiento de las tareas a realizar. Tengo experiencia con el  trabajo bajo presión y estoy en busca de un 
+    empleo donde pueda demostrar mis habilidades. Soy muy responsable, aprendo rápido y tengo muy buena disposición. </p>
+</div>
+
+<div class="Experiencia">
+  <p class="ExperienciaProfesional"> Experiencia Profesional </p>
+  <p class="Puesto"> Puesto Ocupado | Empresa </p>
+  <p class="Pinturas"> Makro-Color </p>
+  <p class="TiempoPinturas"> 2015 - 2019 </p>
+  <p class="LaborPinturas"> Vendedora </p>
+</div>
+
+
+<div class="Educacion">
+  <p class="EducacionTitulo"> Educación </p>
+
+  <p class="TituloColegio"> Grado Bachiller </p>
+  <p class="InfoEducacion"> Presentación Luna Park </p>
+  <p class="InfoEducacion"> 2005 - 2016 </p>
+  
+  <p class="TituloColegio"> Estudiante de Ingeniería Electrónica </p>
+  <p class="InfoEducacion"> Universidad Santo Tomás </p>
+  <p class="InfoEducacion"> Actualmente </p>
+</div>
+
+<div class="Programas">
+  <p class="ProgramasTitulo"> Programas </p>
+
+  <p class="Programa"> Office </p>
+  <p class="Programa"> Visual Studio </p>
+  <p class="Programa"> OrCad </p>
+  <p class="Programa"> MATLAB </p>
+  <p class="Programa"> MPLAB </p>
+  <p class="Programa"> Proteus </p>
+</div>
+
+<div class="Idiomas">
+  <p class="IdiomasTitulo"> Idiomas </p>
+
+  <p class="Idioma"> Español - Nativo </p>
+  <p class="Idioma"> Ingles - Medio-Alto </p>
+  <p class="Idioma"> Frances - Medio </p>
+</div>
+
+<div class="Intereses">
+  <p class="InteresesTitulo"> Intereses </p>
+
+  <p class="Interes"> Lectura </p>
+  <p class="Interes"> Natación </p>
+  <p class="Interes"> Meditación </p>
+  <p class="Interes"> Teatro </p>
+</div>
+
+ </body>
+ </html>
